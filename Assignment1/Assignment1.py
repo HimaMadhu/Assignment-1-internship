a75b5fb58d38dc586150aa46049f50d782f68b45
HimaMadhu <94009528+HimaMadhu@users.noreply.github.com>
2021-11-13 20:59:22 +0530

    Update Assignment1.py

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sympy import *
 
-x1 = symbols('x1')
+x1 = symbols('x₁')
 
 def line_gen(A,B):
   len =10
@@ -17,10 +17,20 @@ A =np.array([x1,2])
 B =np.array([3,4])
 C = 8
 # Equation is ||A-B||=C
+D = A-B
 
-S= list(solveset(Eq((A-B).dot(A-B),C**2),x1))
+# S= list(solveset(Eq((A-B).dot(A-B),C**2),x1))
 
-print("The X1 is: ",S)
+S= list(solveset(Eq(np.dot(np.transpose(D),D),C**2),x1))
+
+print("The x₁ values are: ",S)
+
+#converting to float
+for j in range(len(S)):
+  S[j]=round(float(S[j]),2)
+
+print("The x₁ values in float are: ",S)
+
 labels = ['Solution1','Soultion2']
 j=0
 for i in S:
@@ -29,9 +39,11 @@ for i in S:
   plt.plot(x_FP[0,:],x_FP[1,:],label= labels[j])
   j = j + 1
   plt.plot(P1[0], P1[1], 'o')
-  plt.text(P1[0] * (1 - 0.1), P1[1] * (1 + 0.1), 'A')
+  # plt.text(P1[0] * (1 - 0.1), P1[1] * (1 + 0.1), 'A')
+  plt.text(P1[0] * (1 - 0.1), P1[1] * (1 - 0.5), '({}, {})'.format(P1[0], P1[1]))
   plt.plot(P2[0], P2[1], 'o')
-  plt.text(P2[0] * (1 - 0.1), P2[1] * (1 + 0.1), 'B')
+  # plt.text(P2[0] * (1 - 0.1), P2[1] * (1 + 0.1), 'B')
+  plt.text(P2[0] * (1 - 0.2), P2[1] * (1 + 0.2), '({}, {})'.format(P2[0], P2[1]))
   
 plt.grid()
 plt.xlabel('$x$')
